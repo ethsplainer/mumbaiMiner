0d9d90563c7ef202ed20282776e57ae25339940a
ethsplainer <97244524+ethsplainer@users.noreply.github.com>
2022-04-16 00:01:58 +0200

    final version

--- index.js
+++ index.js
@@ -8,24 +8,27 @@ puppeteer.use(StealthPlugin())
 ;(async () => {
   const browser = await puppeteer.launch({
 	args: ["--no-sandbox"],
-  	headless: false // WIP to make headless work
+  	headless: true
   })
 
-// create new tab & set HTTP headers
+// create new tab & set HTTP headers (to make headless work)
 	const page = await browser.newPage()
 	await page.setExtraHTTPHeaders({
     		'Accept-Language': 'en-US,en;q=0.9'
 		});
 	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
+	
 // navigate to faucet
 	await page.goto('https://faucet.polygon.technology')
 	await page.waitForTimeout(2000)
+	
 // enter wallet and submit request
 	await page.type('input[type=text]', 'your_wallet_here');
 	await page.$eval('button[type=button]', form => form.click() );
 	await page.waitForTimeout(2000)
 	await page.$eval('#app > div > div > div.index > div > div > div:nth-child(1) > div > div.section.position-absolute > div.modal.show > div > div > div:nth-child(2) > div.ps-t-12 > div > button', form => form.click() )
 	await page.waitForTimeout(2000)
+	
 // close tab	
 	await browser.close();
 	
