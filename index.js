34e0f14bbba6dc177a7abdc1112597a7196532a2
Samir <97244524+smramr@users.noreply.github.com>
2022-05-13 20:46:50 +0200

    Loop through list of wallets. Added 5 minute timeout.

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-const puppeteer = require('puppeteer-extra')
+import puppeteer from "puppeteer-extra";
+import addressList from "./wallets.js";
+
+const wallets = addressList;
 
 // stealth plugin
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+puppeteer.use(StealthPlugin());
 
 // IIFE (Immediately Invoked Function Expression)
-;(async () => {
+(async () => {
   const browser = await puppeteer.launch({
-	args: ["--no-sandbox"],
-  	headless: true
-  })
+    args: ["--no-sandbox"],
+    headless: true,
+  });
 
-// create new tab & set HTTP headers (to make headless work)
-	const page = await browser.newPage()
-	await page.setExtraHTTPHeaders({
-    		'Accept-Language': 'en-US,en;q=0.9'
-		});
-	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
-	
-// navigate to faucet
-	await page.goto('https://faucet.polygon.technology')
-	await page.waitForTimeout(2000)
-	
-// enter wallet and submit request
-	await page.type('input[type=text]', 'your_wallet_here');
-	await page.$eval('button[type=button]', form => form.click() );
-	await page.waitForTimeout(2000)
-	await page.$eval('#app > div > div > div.index > div > div > div:nth-child(1) > div > div.section.position-absolute > div.modal.show > div > div > div:nth-child(2) > div.ps-t-12 > div > button', form => form.click() )
-	await page.waitForTimeout(2000)
-	
-// close tab	
-	await browser.close();
-	
-})()
+  // create new tab & set HTTP headers (to make headless work)
+  const page = await browser.newPage();
+  await page.setExtraHTTPHeaders({
+    "Accept-Language": "en-US,en;q=0.9",
+  });
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+  );
+
+  for (let wallet of wallets) {
+    await page.goto("https://faucet.polygon.technology");
+    await page.waitForTimeout(2000);
+    await page.type("input[type=text", wallet);
+    await page.waitForTimeout(2000);
+    await page.$eval("button[type=button]", (form) => form.click());
+    await page.waitForTimeout(2000);
+    await page.$eval(
+      "#app > div > div > div.index > div > div > div:nth-child(1) > div > div.section.position-absolute > div.modal.show > div > div > div:nth-child(2) > div.ps-t-12 > div > button",
+      (form) => form.click()
+    );
+  }
+  await page.waitForTimeout(300000);
+  await browser.close();
+})();
---
+++ wallets.js
@@ -0,0 +1,15 @@
+/* You can add as many wallets as you like. Make sure to enter them correctly.
+
+Example of 4 wallets.
+
+const addressList = [
+    "12332342342342423432",
+    "34523423656532462565",
+    "43459843058302854032",
+    "45234689206206802368"
+];
+*/
+
+const addressList = ["enter wallet here", "more wallets", "are optional"];
+
+export default addressList;
